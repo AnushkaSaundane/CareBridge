e03853b88759911af70246cae32af889f123bb47
Sejal Modak <your-github-email@example.com>
2026-09-10 10:17:59 +0530

    fix: update database generator

--- Database/generate_data.py
+++ Database/generate_data.py
@@ -29,7 +29,7 @@ connection = mysql.connector.connect(
     port=int(os.getenv('DB_PORT', '3306')),
     user=os.getenv('DB_USER', 'root'),
     password=os.getenv('DB_PASSWORD'),
-    database=os.getenv('DB_NAME', 'CareBridge')
+    database=os.getenv('DB_NAME', 'carebridge')
 )
 
 cursor = connection.cursor()
